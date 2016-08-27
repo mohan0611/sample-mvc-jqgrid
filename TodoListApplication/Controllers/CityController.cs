684123b1420d26b2a7bac917152bc6b565d211b5
mohan0611 <mohan0611@gmail.com>
2016-08-27 19:31:14 +0530

    the city grid is added

---
+++ TodoListApplication/Controllers/CityController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+//using TodoListApplication.DBContext;
+//using TodoListApplication.Models;
+using TodoListApplication.Entity;
+
+
+namespace TodoListApplication.Controllers
+{
+    public class CityController : Controller
+    {
+        TodoListEntities db = new TodoListEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //To get the list of City for the given country
+        public JsonResult GetCityLists(string sidx, string sord, int page, int rows, int countryid)  
+        {
+            int pageIndex = Convert.ToInt32(page) - 1;
+            int pageSize = rows;
+            var cityresult = db.Cities.Select(
+                    a => new
+                        {   a.CityID,
+                            a.CityName, 
+                            a.Country.CountryName,
+                            a.TotalPopulation
+                        });
+            int totalRecords = cityresult.Count();
+            var totalPages = (int)Math.Ceiling((float)totalRecords / (float)rows);
+            if (sord.ToUpper() == "DESC")
+            {
+                cityresult = cityresult.OrderByDescending(s => s.CityName);
+                cityresult = cityresult.Skip(pageIndex * pageSize).Take(pageSize);
+            }
+            else
+            {
+                cityresult = cityresult.OrderBy(s => s.CityName);
+                cityresult = cityresult.Skip(pageIndex * pageSize).Take(pageSize);
+            }
+            var jsonData = new{
+                 total = totalPages,
+                 page,
+                 records = totalRecords,
+                 rows = cityresult
+            };
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+        }
+
+        // TODO:insert a new row to the grid logic here
+        [HttpPost]
+        public string Create([Bind(Exclude = "Id")] City objCity)
+        {
+            string msg;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Cities.Add(objCity);
+                    db.SaveChanges();
+                    msg = "Saved Successfully";
+                }
+                else
+                {
+                    msg = "Validation data not successfull";
+                }
+            }
+            catch(Exception ex)
+            {
+                msg = "Error occured:" + ex.Message;
+            }
+            return msg;
+        }
+        public string Edit(City objCities)
+        {
+            string msg;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(objCities).State = EntityState.Modified;
+                    db.SaveChanges();
+                    msg = "Saved Successfully";
+                }
+                else
+                {
+                    msg = "Validation data not successfull";
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = "Error occured:" + ex.Message;
+            }
+            return msg;
+        }
+        public string Delete(int Id)
+        {
+            City objCity = db.Cities.Find(Id);
+            db.Cities.Remove(objCity);
+            db.SaveChanges();
+            return "Deleted successfully";
+        }
+    }
+}
--- TodoListApplication/Scripts/app/countrygrid.js
+++ TodoListApplication/Scripts/app/countrygrid.js
@@ -1,80 +1,115 @@
-﻿$(function () {
-    $("#country").jqGrid({
-        url: "/Country/GetCountryLists",
-        datatype: 'json',
-        mtype: 'Get',
-        colNames: ['CountryId', 'Country Name', 'Language'],
-        colModel: [
-            { key: true, hidden: true, name: 'CountryId', index: 'CountryId', editable: true },
-            { key: false, name: 'CountryName', index: 'CountryName', editable: true },
-            { key: false, name: 'Primarylanguage', index: 'Primarylanguage', editable: true },
-            ],
-        pager: jQuery('#pager'),
-        rowNum: 10,
-        rowList: [10, 20, 30, 40], 
-        height: '100%',
-        viewrecords: true,
-        caption: 'Country',
-        emptyrecords: 'No records to display',
-        jsonReader: {
-            root: "rows",
-            page: "page",
-            total: "total",
-            records: "records",
-            repeatitems: false,
-            Id: "0"
-        },
-        autowidth: true,
-        multiselect: false
-    }).navGrid('#pager', { edit: true, add: true, del: true, search: false, refresh: true },
-        {
-            // edit options
-            zIndex: 100,
-            url: '/Country/Edit',
-            closeOnEscape: true,
-            closeAfterEdit: true,
-            recreateForm: true,
-            beforeSubmit: function (postdata, formid) {
-                if ($('#TaskName').val() == "") {
-                    $('#TaskName').addClass("ui-state-highlight");
-                    return [false, 'ERROR MESSAGE']; //error
-                }
-                return [true, ''];
-            },
-            afterComplete: function (response) {
-                if (response.responseText) {
-                    alert(response.responseText);
-                }
-            },
-            width: 600,
-            height:300
-        },
-        {
-            // add options
-            zIndex: 100,
-            url: "/Country/Create",
-            closeOnEscape: true,
-            closeAfterAdd: true,
-            afterComplete: function (response) {
-                if (response.responseText) {
-                    alert(response.responseText);
-                }
-            },
-            width: 600,
-            height:300
-        },
-        {
-            // delete options
-            zIndex: 100,
-            url: "/Country/Delete",
-            closeOnEscape: true,
-            closeAfterDelete: true,
-            recreateForm: true,
-            msg: "Are you sure you want to delete this task?",
-            afterComplete: function (response) {
-                if (response.responseText) {
-                    alert(response.responseText);
-                }
+﻿jQuery("#country").jqGrid({
+    url: "/Country/GetCountryLists",
+    datatype: 'json',
+    mtype: 'Get',
+    colNames: ['CountryId', 'Country Name', 'Language'],
+    colModel: [
+        { key: true, hidden: true, name: 'CountryId', index: 'CountryId', editable: true },
+        { key: false, name: 'CountryName', index: 'CountryName', editable: true },
+        { key: false, name: 'Primarylanguage', index: 'Primarylanguage', editable: true },
+    ],
+    pager: jQuery('#pager'),
+    rowNum: 10,
+    rowList: [10, 20, 30, 40],
+    height: '100%',
+    viewrecords: true,
+    caption: 'Country',
+    emptyrecords: 'No records to display',
+    jsonReader: {
+        root: "rows",
+        page: "page",
+        total: "total",
+        records: "records",
+        repeatitems: false,
+        Id: "0"
+    },
+    autowidth: true,
+    multiselect: false,
+    onSelectRow: function (ids) {
+        //$('#countrynamedisp').val(ids);
+        if (ids == null) {
+            ids = 0;
+            if (jQuery("#city").jqGrid('getGridParam', 'records') > 0) {
+                jQuery("#city").jqGrid('setGridParam', { url: "/City/GetCityLists?countryid=" + ids, page: 1 });
+                jQuery("#city").jqGrid('setCaption', "City for country")
+                .trigger('reloadGrid');
             }
-        });
-});
+        } else {
+            jQuery("#city").jqGrid('setGridParam', { url: "/City/GetCityLists?countryid=" + ids, page: 1 });
+            jQuery("#city").jqGrid('setCaption', "Invoice Detail: " + ids)
+            .trigger('reloadGrid');
+        }
+    }
+}).navGrid('#pager', { edit: true, add: true, del: true, search: false, refresh: true },
+    {
+        // edit options
+        zIndex: 100,
+        url: '/Country/Edit',
+        closeOnEscape: true,
+        closeAfterEdit: true,
+        recreateForm: true,
+        beforeSubmit: function (postdata, formid) {
+            if ($('#TaskName').val() == "") {
+                $('#TaskName').addClass("ui-state-highlight");
+                return [false, 'ERROR MESSAGE']; //error
+            }
+            return [true, ''];
+        },
+        afterComplete: function (response) {
+            if (response.responseText) {
+                alert(response.responseText);
+            }
+        },
+        width: 600,
+        height: 300
+    },
+    {
+        // add options
+        zIndex: 100,
+        url: "/Country/Create",
+        closeOnEscape: true,
+        closeAfterAdd: true,
+        afterComplete: function (response) {
+            if (response.responseText) {
+                alert(response.responseText);
+            }
+        },
+        width: 600,
+        height: 300
+    },
+    {
+        // delete options
+        zIndex: 100,
+        url: "/Country/Delete",
+        closeOnEscape: true,
+        closeAfterDelete: true,
+        recreateForm: true,
+        msg: "Are you sure you want to delete this country?",
+        afterComplete: function (response) {
+            if (response.responseText) {
+                alert(response.responseText);
+            }
+        }
+    });
+
+jQuery("#city").jqGrid({
+    height: 100,
+    url: '/City/GetCityLists?countryid=0',
+    datatype: "json",
+    colNames: ['City ID', 'City Name', 'Country Name', 'Population'],
+    colModel: [
+   		{ name: 'CityID', index: 'CityID', width: 55 },
+   		{ name: 'CityName', index: 'CityName', width: 180 },
+   		{ name: 'CountryName', index: 'CountryName', width: 80 },
+   		{ name: 'TotalPopulation', index: 'TotalPopulation', width: 80, align: "right" }
+   		//{ name: 'linetotal', index: 'linetotal', width: 80, align: "right", sortable: false, search: false }
+    ],
+    rowNum: 5,
+    rowList: [5, 10, 20],
+    pager: '#pager_city',
+    sortname: 'CityName',
+    viewrecords: true,
+    sortorder: "asc",
+    //multiselect: true,
+    caption: "City"
+}).navGrid('#pager_city', { add: false, edit: false, del: false });
